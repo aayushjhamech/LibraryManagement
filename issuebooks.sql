393dff40773b6122cd013d29346c16df62a75c17
aayushjhamech <36695172+aayushjhamech@users.noreply.github.com>
2020-07-10 23:12:59 +0530

    Update issuebooks.sql

--- issuebooks.sql
+++ issuebooks.sql
@@ -1,30 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 3.4.5
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Jul 19, 2016 at 07:40 PM
--- Server version: 5.5.16
--- PHP Version: 5.4.0beta2-dev
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `test`
---
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `issuebooks`
---
+
 
 CREATE TABLE IF NOT EXISTS `issuebooks` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -45,6 +21,4 @@ INSERT INTO `issuebooks` (`id`, `bookcallno`, `studentid`, `studentname`, `stude
 (6, 'A@4', 335, 'Sumedh', '95676565756', '2016-07-19 18:44:34'),
 (7, 'A@4', 87, 'abhishek', '9329882382', '2016-07-19 18:46:12');
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
